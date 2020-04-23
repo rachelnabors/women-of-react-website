a4308fe699a43ca72563fe75c897345ddd74412e
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2020-04-23 15:02:46 +0100

    Update index.js
    As mentioned on https://github.com/rachelnabors/women-of-react-website/pull/4
    
    > For consistency with other sections. Would also fix the "syntax error" :-)

--- src/pages/index.js
+++ src/pages/index.js
@@ -346,7 +346,7 @@ const IndexPage = () => {
           <Speakers />
         </ImageGrid>
       </Card>
-      <Card header="Special Guests">
+      <Card header="SpecialGuests">
         <ImageGrid className="folks">
           <SpecialGuests />
         </ImageGrid>
